d46f21caa7732ff22a3e3215ba1ccf19d8283b5f
JashwanthSA <jash.chn@gmail.com>
2024-04-09 19:44:19 +0530

    adding quiz-react

---
+++ quiz-react/src/components/Score.jsx
@@ -0,0 +1,14 @@
+class Score extends Component {
+    render() {
+        const { score, onNextQuestion } = this.props;
+ 
+        return (
+            <div>
+                <h2>Results</h2>
+                <h4>Your score: {score}</h4>
+            </div>
+        );
+    }
+}
+ 
+export default Score;
